d7bf9dfaccd57b72d5cd5ee79a51c230622807b6
Aditya Mittal <adityamittal2410@gmail.com>
2025-01-06 19:10:59 +0530

    fix header

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -11,12 +11,12 @@ function Header() {
     },[]);
   return (
     <div className="flex py-4 px-6 items-center justify-between bg-secondary shadow-sm">
-      <Image src={"/logo.svg"} width={100} height={40} alt="logo" />
+      <Image src={"/logo.svg"} width={80} height={40} alt="logo" />
       <ul className="hidden md:flex gap-6">
         <li className={`hover:text-violet-700 transition-all cursor-pointer ${path=='/dashboard'&&'text-violet-700'}`}>Dashboard</li>
         <li className={`hover:text-violet-700 transition-all cursor-pointer ${path=='/upgrade'&&'text-violet-700'}`}>Upgrade</li>
         <li className={`hover:text-violet-700 transition-all cursor-pointer ${path=='/questions'&&'text-violet-700'}`}>Questions</li>
-        <li className={`hover:text-violet-700 transition-all cursor-pointer ${path=='/works'&&'text-violet-700'}`}>How it works?</li>
+        <li className={`hover:text-violet-700 transition-all cursor-pointer ${path=='/working'&&'text-violet-700'}`}>How it works?</li>
       </ul>
       <UserButton />
     </div>
