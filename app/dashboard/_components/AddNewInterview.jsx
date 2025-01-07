0a74894b18b7609b34a8eec1c74fe8ffd3c9d44d
Aditya Mittal <adityamittal2410@gmail.com>
2025-01-07 20:23:44 +0530

    establish communication with gemini api and saving the response in db

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -12,15 +12,66 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { chatSession } from "@/utils/GeminiAIModel";
+import { LoaderCircle } from "lucide-react";
+import { db } from "@/utils/db";
+import { MockInterview } from "@/utils/schema";
+import { v4 as uuidv4 } from "uuid";
+import { useUser } from "@clerk/nextjs";
+import moment from "moment";
 
 function AddNewInterview() {
   const [openDialog, setOpenDialog] = useState(false);
   const [jobPosition, setJobPosition] = useState();
   const [jobDesc, setJobDesc] = useState();
   const [jobExperience, setJobExperience] = useState();
-  const onSubmit = (e) => {
+  const [loading, setLoading] = useState(false);
+  const [jsonResponse, setJsonResponse] = useState([]);
+  const { user } = useUser();
+
+  const onSubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
-    console.log(jobExperience, jobDesc, jobPosition);
+
+    const inputPrompt =
+      "A candidate's professional information(Job Position, Job Description, Experience) is delimited in triple backticks. Make " +
+      process.env.NEXT_PUBLIC_INTERVIEW_QUESTION_COUNT +
+      " real life interview questions based on the details along with their answers. Send the response in JSON format. Give question and answer field in JSON. ```Job Position: " +
+      jobPosition +
+      ", Job Description/Tech Stack: " +
+      jobDesc +
+      ", Experience: " +
+      jobExperience +
+      " ```";
+
+    const result = await chatSession.sendMessage(inputPrompt);
+    const jsonResp = result.response
+      .text()
+      .replace("```json", "")
+      .replace("```", "");
+    console.log(JSON.parse(jsonResp));
+    setJsonResponse(jsonResp);
+
+    if (jsonResp) {
+      const resp = await db
+        .insert(MockInterview)
+        .values({
+          mockId: uuidv4(),
+          jsonMockResp: jsonResp,
+          jobPosition: jobPosition,
+          jobDesc: jobDesc,
+          jobExperience: jobExperience,
+          createdBy: user?.primaryEmailAddress?.emailAddress,
+          createdAt: moment().format("DD-MM-yyyy"),
+        })
+        .returning({ mockId: MockInterview.mockId });
+
+      console.log("Inserted ID:", resp);
+    } else {
+      console.log("Error");
+    }
+
+    setLoading(false);
   };
   return (
     <div>
@@ -62,7 +113,7 @@ function AddNewInterview() {
             <div className="">
               <label>Years of experience</label>
               <Input
-                placeholder="5"
+                placeholder="0-40"
                 type="number"
                 max="40"
                 required
@@ -78,7 +129,16 @@ function AddNewInterview() {
                 >
                   Cancel
                 </Button>
-                <Button type="submit">Start Interview</Button>
+                <Button type="submit" disable={loading.toString()}>
+                  {loading ? (
+                    <>
+                      <LoaderCircle className="animate-spin" /> Preparing
+                      Interview
+                    </>
+                  ) : (
+                    "Start Interview"
+                  )}
+                </Button>
               </div>
             </DialogFooter>
           </form>
