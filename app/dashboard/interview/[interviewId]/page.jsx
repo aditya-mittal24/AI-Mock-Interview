3441adf6cd13d2168442d69671e205aca1b8295c
Aditya Mittal <adityamittal2410@gmail.com>
2025-01-09 00:55:52 +0530

    complete before-interview page

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -6,8 +6,7 @@ import { eq } from "drizzle-orm";
 import { Lightbulb, WebcamIcon } from "lucide-react";
 import * as React from "react";
 import Webcam from "react-webcam";
-import { Skeleton } from "@/components/ui/skeleton"
-
+import { Skeleton } from "@/components/ui/skeleton";
 
 function Interview({ params }) {
   const { interviewId } = React.use(params);
@@ -34,25 +33,37 @@ function Interview({ params }) {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         <div className="flex flex-col my-5 gap-5">
           <div className="flex flex-col p-5 rounded-lg border gap-5">
-            <h2 className="text-lg">
-              <strong>Job Position:</strong>{" "}
-              {interviewData ? interviewData.jobPosition : <Skeleton className="h-4 w-[200px] text-red-600" />}
+            <h2 className="text-lg flex gap-2 items-center">
+              <strong>Job Position:</strong>
+              {interviewData.jobPosition ? (
+                interviewData.jobPosition
+              ) : (
+                <Skeleton className="h-4 w-[200px]" />
+              )}
             </h2>
-            <h2 className="text-lg">
-              <strong>Job Description/Tech Stack:</strong>{" "}
-              {interviewData ? interviewData.jobDesc : ""}
+            <h2 className="text-lg flex gap-2 items-center">
+              <strong>Job Description/Tech Stack:</strong>
+              {interviewData.jobDesc ? (
+                interviewData.jobDesc
+              ) : (
+                <Skeleton className="h-4 w-[200px]" />
+              )}
             </h2>
-            <h2 className="text-lg">
-              <strong>Experience:</strong>{" "}
-              {interviewData ? interviewData.jobExperience : ""}
+            <h2 className="text-lg flex gap-2 items-center">
+              <strong>Experience:</strong>
+              {interviewData.jobExperience ? (
+                interviewData.jobExperience
+              ) : (
+                <Skeleton className="h-4 w-[200px]" />
+              )}
             </h2>
           </div>
-          <div>
-            <div className="flex gap-2 items-center">
+          <div className="p-5 border rounded-lg border-yellow-300 bg-yellow-100">
+            <div className="flex gap-2 items-center text-yellow-500">
               <Lightbulb />
               <strong>Important Instructions</strong>
             </div>
-            <ol className="list-decimal">
+            <ol className="list-decimal my-2">
               <li>
                 Please ensure your webcam and microphone are enabled for the
                 session.
@@ -63,9 +74,10 @@ function Interview({ params }) {
               </li>
               <li>
                 During the interview:
-                <ul className="list-disc">
-                  <li>The AI interviewer will ask you a series</li>
-                  of questions.
+                <ul className="list-disc ml-3">
+                  <li>
+                    The AI interviewer will ask you a series of questions.
+                  </li>
                   <li>
                     Provide clear and thoughtful responses, as these will be
                     analyzed to generate a detailed report.
@@ -73,10 +85,10 @@ function Interview({ params }) {
                 </ul>
               </li>
             </ol>
-            <h2>
-              Note: Your video and audio data will not be stored at any point.
-              This ensures your privacy and data security. Good luck with your
-              interview!
+            <h2 className="mt-4">
+              <strong>Note:</strong> Your video and audio data will not be
+              stored at any point. This ensures your privacy and data security.
+              Good luck with your interview!
             </h2>
           </div>
         </div>
@@ -86,10 +98,7 @@ function Interview({ params }) {
               onUserMedia={() => setWebcamEnabled(true)}
               onUserMediaError={() => setWebcamEnabled(false)}
               mirrored={true}
-              style={{
-                height: 300,
-                width: 300,
-              }}
+              
             />
           ) : (
             <div className="flex flex-col">
@@ -100,6 +109,9 @@ function Interview({ params }) {
             </div>
           )}
         </div>
+      </div>
+      <div className="flex justify-end items-end">
+        <Button disabled={!webcamEnabled}>Start Interview</Button>
       </div>
     </div>
   );
