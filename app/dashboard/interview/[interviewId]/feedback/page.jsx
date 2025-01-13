d1bef02506144761dc17db0dc1f9a14a18eac91c
Aditya Mittal <adityamittal2410@gmail.com>
2025-01-13 18:08:29 +0530

    show overall score

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Feedback() {
   return (
-    <div>Feedback</div>
+    <div className='p-10'>
+    <h2 className='text-3xl font-bold text-green-500'>Congratulations!</h2>
+    <h2 className='font-bold text-2xl'>Here is you interview feedback</h2>
+    <h2 className='text-primary text-lg my-3'>You overall interview rating: <strong>7/1-</strong></h2>
+    </div>
   )
 }
 
