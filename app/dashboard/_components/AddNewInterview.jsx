1e351739c00f4c8c6443f08f494d4fc2351009d2
Aditya Mittal <adityamittal2410@gmail.com>
2025-01-14 01:15:53 +0530

    add interview round radio group

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -9,11 +9,18 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { chatSession } from "@/utils/GeminiAIModel";
-import { LoaderCircle } from "lucide-react";
+import { Info, LoaderCircle } from "lucide-react";
 import { db } from "@/utils/db";
 import { MockInterview } from "@/utils/schema";
 import { v4 as uuidv4 } from "uuid";
@@ -26,6 +33,7 @@ function AddNewInterview() {
   const [jobPosition, setJobPosition] = useState();
   const [jobDesc, setJobDesc] = useState();
   const [jobExperience, setJobExperience] = useState();
+  const [interviewRound, setInterviewRound] = useState();
   const [loading, setLoading] = useState(false);
   const [jsonResponse, setJsonResponse] = useState([]);
   const { user } = useUser();
@@ -34,6 +42,7 @@ function AddNewInterview() {
   const onSubmit = async (e) => {
     setLoading(true);
     e.preventDefault();
+    console.log();
 
     const inputPrompt =
       "A candidate's professional information(Job Position, Job Description, Experience) is delimited in triple backticks. Make " +
@@ -70,8 +79,8 @@ function AddNewInterview() {
 
       console.log("Inserted ID:", resp);
       if (resp) {
-        setOpenDialog(false)
-        router.push('/interview/' + resp[0]?.mockId)
+        setOpenDialog(false);
+        router.push("/interview/" + resp[0]?.mockId);
       }
     } else {
       console.log("Error");
@@ -99,7 +108,96 @@ function AddNewInterview() {
               years of experience
             </DialogDescription>
           </DialogHeader>
-          <form className="flex flex-col gap-y-4 mt-1" onSubmit={onSubmit}>
+          <form className="flex flex-col gap-y-5 mt-1" onSubmit={onSubmit}>
+            <div className="">
+              <label>Interview Round</label>
+              <RadioGroup
+                onChange={(event) => {
+                  console.log(event.target.value)
+                  setInterviewRound(event.target.value);
+                }}
+              >
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem
+                    value="Technical Round"
+                    id="Technical Round"
+                  />
+                  <label
+                    htmlFor="Technical Round"
+                    className="flex gap-1 items-center"
+                  >
+                    Technical Round
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <Info className="h-4 w-4" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Programming and DSA concepts. No coding.</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="Coding Round" id="Coding Round" />
+                  <label
+                    htmlFor="Coding Round"
+                    className="flex gap-1 items-center"
+                  >
+                    Coding Round
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <Info className="h-4 w-4" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>1-2 DSA questions. Coding + concepts.</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem
+                    value="Technical + Coding Round"
+                    id="Technical + Coding Round"
+                  />
+                  <label
+                    htmlFor="Technical + Coding Round"
+                    className="flex gap-1 items-center"
+                  >
+                    Technical + Coding Round
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <Info className="h-4 w-4" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Technical questions + coding topics</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="HR Round" id="HR Round" />
+                  <label htmlFor="HR Round" className="flex gap-1 items-center">
+                    HR Round
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <Info className="h-4 w-4" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>HR questions only. No technical questions.</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </label>
+                </div>
+              </RadioGroup>
+            </div>
             <div className="">
               <label>Job Role/Job Position</label>
               <Input
