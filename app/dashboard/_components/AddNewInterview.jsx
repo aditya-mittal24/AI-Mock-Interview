52bc295bafbdf5125273843f06179cef2570f718
Aditya Mittal <adityamittal2410@gmail.com>
2025-01-18 10:13:43 +0530

    Add onOpenChange to Dialog

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -218,7 +218,7 @@ function AddNewInterview() {
       >
         <h2 className="text-lg text-center">+ Add New</h2>
       </div>
-      <Dialog open={openDialog} close={openDialog}>
+      <Dialog open={openDialog} onOpenChange={setOpenDialog}>
         <DialogContent className="max-w-2xl">
           <DialogHeader>
             <DialogTitle className="text-2xl">
